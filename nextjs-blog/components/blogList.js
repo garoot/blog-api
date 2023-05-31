23ad390069503cfb39beffa2496e261270751cf3
Abdulmajeed <majeed.garoot@gmail.com>
2023-05-31 23:55:50 +1000

    + Separated blogCard outside blogList component

--- nextjs-blog/components/blogList.js
+++ nextjs-blog/components/blogList.js
@@ -1,6 +1,7 @@
 import styles from './blogList.module.css'
 import Link from 'next/link';
 import Image from 'next/image';
+import BlogCard from './blogCard';
 
 export default function BlogList({ Component, pageProps }) {
     return (
@@ -22,43 +23,7 @@ export default function BlogList({ Component, pageProps }) {
             </div>
 
             <div className={styles.blogsContainer}>
-                <div className={styles.blogCard}>
-                    
-                    <div className={styles.cardThumbnailContainer}>
-                        <img className={styles.thumbnail} src="/images/image.png" alt="" />
-                    </div>
-
-                    <div className={styles.blogCardRight}>
-                        <div className={styles.cardTopicSave}>
-                            <h3>Panda Quick Guide</h3>
-                            <a href="">
-                                <img src="/icons/save.png" alt="" />
-                            </a>
-                        </div>
-                        <div className={styles.cardDesc}>
-                            <body>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, sint.
-                            </body>
-
-                        </div>
-                        <div className={styles.cardViewsSavesLikes}>
-                            <span>
-                                <img src="/icons/view.png" alt="views" />
-                                <p>1.2k</p>
-                            </span>
-                            <span>
-                                <img src="/icons/saved.png" alt="bookmarks" />
-                                <p>112</p>
-                            </span>
-                            <span>
-                                <img src="/icons/like.png" alt="likes" />
-                                <p>400</p>
-                            </span>
-
-                        </div>
-                    </div>
-                    
-                </div>
+                <BlogCard/>
             </div>
 
         </div>
