976ca2dd35a66fd41075b4d639f24bbcdd1118c0
Abdulmajeed <majeed.garoot@gmail.com>
2023-06-28 00:37:03 +1000

    + handled toggle between login and register forms in auth.js

--- nextjs-2-blog/src/app/auth/page.js
+++ nextjs-2-blog/src/app/auth/page.js
@@ -1,14 +1,46 @@
+"use client"
+
+import { useEffect, useRef, useState } from 'react'
 import styles from './auth.module.css'
 
 export default function AuthWindow({ Component, pageProps }) {
+
+    const [showLogin, setShowLogin] = useState(true)
+    const [showRegister, setShowRegister] = useState(false)
+    const activeLogin = useRef(null)
+    const activeRegister = useRef(null)
+
+    const openLoginForm = () => {
+        if(!showLogin){
+            setShowLogin(!showLogin)
+            setShowRegister(false)
+        }
+    }
+    const openRegisterForm = () => {
+        if(!showRegister){
+            setShowRegister(!showRegister)
+            setShowLogin(false)
+        }
+    }
+
+    // useEffect(()=> {
+    //     activeLogin.current.focus()
+    // })
+
+    // useEffect(() => {
+    //     console.log(showLogin)
+    //     console.log(showRegister)
+    // }, [showLogin, showRegister])
+
+
     return (
         <div className={styles.container}>
             <div className={styles.popup}>
                 <div className={styles.buttons}>
-                    <button id={styles.registerButton}>Register</button>
-                    <button id={styles.login}>Login</button>
+                    <button ref={activeRegister} onClick={()=> openRegisterForm()} id={styles.registerButton}>Register</button>
+                    <button ref={activeLogin}  onClick={()=> openLoginForm()} id={styles.login}>Login</button>
                 </div>
-                <div className={styles.socialAuth}>
+                <div className={styles.loginSocialAuth}>
                     <button id={styles.google}>
                         <img src="/images/Google.png" alt="" />
                     </button>
@@ -16,16 +48,37 @@ export default function AuthWindow({ Component, pageProps }) {
                         <img src="/images/facebook.png" alt="" />
                     </button>
                 </div>
-                <form className={styles.loginForm} action="">
-                    <label htmlFor="userName">
-                        <input type="text" name="username" id="userName" placeholder='Email Address'/>
-                    </label>
-                    <label htmlFor="password">
-                        <input type="text" name="password" id="password" placeholder='Password'/>
-                    </label>
-                    <button className={styles.submitButton} type='submit'>Login</button>
-                </form>
-
+                {
+                showLogin? (
+                    <form className={styles.loginForm} action="">
+                        <label htmlFor="userName">
+                            <input type="text" name="emailAddress" id="emailAddress" placeholder='Email Address'/>
+                        </label>
+                        <label htmlFor="password">
+                            <input type="text" name="password" id="password" placeholder='Password'/>
+                        </label>
+                        <button className={styles.submitButton} type='submit'>Login</button>
+                    </form>
+                ):('')
+                }
+                
+                {
+                showRegister? (
+                    <form action="" className={styles.registerForm}>
+                        <label htmlFor="emailAddress">
+                            <input type="text" name="emailAddress" id="emailAddress" placeholder='Email Address'/>
+                        </label>
+                        <label htmlFor="password">
+                            <input type="text" name="password" id="password" placeholder='Password'/>
+                        </label>
+                        <label htmlFor="password2">
+                            <input type="text" name="password2" id="password2" placeholder='Repeat your password'/>
+                        </label>
+                        <button className={styles.submitButton} type='submit'>Register</button>
+                    </form>
+                ):('')
+                }
+                
             </div>
         </div>
 
